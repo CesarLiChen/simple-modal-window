0c05c1fe4b705375e3c5fcc3a21f1b21b880a8cf
Cesar Li Chen <cesarli619@gmail.com>
2023-05-28 16:54:15 -0400

    Completes event handling

--- main.js
+++ main.js
@@ -5,4 +5,23 @@ const modal = document.querySelector("#my-modal");
 
 const modalButton = document.querySelector("#modal-button");
 
-const closeButton = document.querySelector("#close");
+const closeButton = document.querySelector(".close");
+
+// Events
+modalButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+function outsideClick(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
