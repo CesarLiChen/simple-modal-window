6dca866de010fa969740525f81752ccb676a57cb
Cesar Li Chen <cesarli619@gmail.com>
2023-05-27 23:18:05 -0400

    Gets dom elements via javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Js working");
+
+// Get DOM elements.
+const modal = document.querySelector("#my-modal");
+
+const modalButton = document.querySelector("#modal-button");
+
+const closeButton = document.querySelector("#close");
